4e4ecb0508b8171fd2f68e5609e3a8667a30a6af
Ashrafulislam1211 <Emonhmed554@gmail.com>
2025-08-28 00:11:36 -0400

    Add new SCSS files (home.scss, mixins.scss, people.scss, app.scss) and update App.jsx, _global-imports.scss, _variables.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-import MyFirstComponent from './Components/MyFirstComponent'
-import Map from './Components/Map'
-import ControlledInput from './Components/ControlledInput'
-import SideEffect from './Components/SideEffect'
-import ProgramList from './Components/ProgramList'
-import Carousel from './Components/Carousel'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './app.scss';
+import MyFirstComponent from './Components/MyFirstComponent';
+import Map from './Components/Map';
+import ControlledInput from './Components/ControlledInput';
+import SideEffect from './Components/SideEffect';
+import ProgramList from './Components/ProgramList';
+import Carousel from './Components/Carousel';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import { Route, Routes } from 'react-router-dom'
-import Leadership from './Components/leadership'
-import Home from './Components/home'
-import Navigation from './Components/Navigation'
-import People from './Components/people'
-import SingleUser from './Components/singleUser'
-import UsingAxios from './Components/UsingAxios'
+import { Route, Routes } from 'react-router-dom';
+import Leadership from './Components/leadership';
+import Home from './Components/home';
+import Navigation from './Components/Navigation';
+import People from './Components/people';
+import SingleUser from './Components/singleUser';
+import UsingAxios from './Components/UsingAxios';
 
 
 
