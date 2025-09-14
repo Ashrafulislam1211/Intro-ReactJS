0925d3984fea0eb5aeb43dd3bb7cbc35545e6096
Ashrafulislam1211 <Emonhmed554@gmail.com>
2025-09-14 00:50:45 -0400

    Add new folder and files

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='container'>
+        <h1>Shop with confidence: Trusted Source for Quality Products</h1>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/Components/productDelivery.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const productDelivery = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default productDelivery
